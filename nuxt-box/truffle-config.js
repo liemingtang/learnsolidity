de10ccb441bdc106ec601c03cdf3e426b932ade4
Lie Ming Tang <liemingtang@gmail.com>
2020-11-22 23:47:49 +1100

    iworking

--- nuxt-box/truffle-config.js
+++ nuxt-box/truffle-config.js
@@ -2,6 +2,13 @@ module.exports = {
 	// See <http://truffleframework.com/docs/advanced/configuration>
 	// to customize your Truffle configuration!
 	  // Configure your compilers
+	  networks: {
+		development: {
+		  host: "127.0.0.1",
+		  port: 7545,
+		  network_id: "*"
+		}
+	  },
 	  compilers: {
 		solc: {
 		  version: "0.4.21",    // Fetch exact version from solc-bin (default: truffle's version)
